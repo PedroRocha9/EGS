52364589a22e86126fb35c97b269423d459565e5
andreclerigo <andreclerigo@outlook.com>
2023-03-05 02:30:21 +0000

    starting to create the notion of an user

--- social-api/index.js
+++ social-api/index.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const swaggerUI = require('swagger-ui-express');
 const swaggerJsDoc = require('swagger-jsdoc');
 const cors = require('cors');
+const path = require('path');
 
 const static = require('./static');
 const package = require('./package.json');
@@ -15,14 +16,14 @@ const URL = PROTOCOL + "://" + IP + (process.env.PORT ? "" : ":" + PORT);
 
 
 
-// cors middleware
-app.use(cors({origin:true,credentials: true}));
+// Expose public content
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
-// enable CORS for preflight operations
+// Enable Cors Middleware
+app.use(cors({origin:true,credentials: true}));
 app.options('*', cors());
 
-
-// setup swagger docs
+// Setup Swagger Docs
 const swagger_options = {
   definition: {
       openapi: "3.0.3",
@@ -45,21 +46,23 @@ const swagger_options = {
   apis: ["./routes/*.js", "./docs/mixit-social-api.yml"],
 }
 
+// Swagger Docs customization
 const swagger_specs = swaggerJsDoc(swagger_options);
-app.use("/docs", swaggerUI.serve, swaggerUI.setup(swagger_specs, {
-    customSiteTitle: "MIXIT Social API"
+app.use("/v1/docs", swaggerUI.serve, swaggerUI.setup(swagger_specs, {
+    customSiteTitle: "MIXIT Social API",
+    customfavIcon: "/public/mixit-favicon.png"
 }));
 
+// Redirect API root to docs webpage
 app.get('/', (req, res) => {
-  // temporarily redirect to /docs
-  res.redirect('/docs'); 
+  res.redirect('/v1/docs'); 
 })
 
 // Setup routes
 fs.readdir(static.ROUTES_DIR, (err, files) => {
-  files.forEach(file => app.use(`/${file.replace(".js", "")}`, require(static.ROUTES_DIR + file.replace(".js", ""))))
+  files.forEach(file => app.use(`/v1/${file.replace(".js", "")}`, require(static.ROUTES_DIR + file.replace(".js", ""))))
 
-  // middleware to handle 404 when nothing else responded
+  // Middleware to handle 404 when nothing else responded
   app.use((req, res, next) => {
       res.status(404).json({
           "message": `404 | Endpoint ${req.url} Not Found!`,
@@ -71,8 +74,7 @@ fs.readdir(static.ROUTES_DIR, (err, files) => {
   });
 });
 
+// Startup Message
 app.listen(PORT, () => {
   console.log(`App listening on ${URL}`)
 })
-
-
